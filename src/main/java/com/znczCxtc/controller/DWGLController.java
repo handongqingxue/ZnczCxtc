47570ef52fa564e9ac328d98f760153f99bb0ceb
handongqingxue <1417140290@qq.com>
2022-09-29 16:23:16 +0800

    完善

--- src/main/java/com/znczCxtc/controller/DWGLController.java
+++ src/main/java/com/znczCxtc/controller/DWGLController.java
@@ -15,18 +15,62 @@ import com.znczCxtc.entity.*;
 import com.znczCxtc.service.*;
 import com.znczCxtc.util.JsonUtil;
 import com.znczCxtc.util.PlanResult;
+import com.znczLfylJhb.entity.YunShuShang;
 
 @Controller
 @RequestMapping("/"+DWGLController.MODULE_NAME)
 public class DWGLController {
 
-	//@Autowired
-	//private YunShuShangService yunShuShangService;
+	@Autowired
+	private YunShuShangService yunShuShangService;
 	@Autowired
 	private FaHuoDanWeiService faHuoDanWeiService;
 	@Autowired
     private ShouHuoDanWeiService shouHuoDanWeiService;
 	public static final String MODULE_NAME="dwgl";
+
+	@RequestMapping(value="/yss/new")
+	public String goYssNew(HttpServletRequest request) {
+		
+		//publicService.selectNav(request);
+		
+		return MODULE_NAME+"/yss/new";
+	}
+
+	@RequestMapping(value="/yss/edit")
+	public String goYssEdit(HttpServletRequest request) {
+		
+		//publicService.selectNav(request);
+		String id = request.getParameter("id");
+		YunShuShang yss=yunShuShangService.selectById(id);
+		request.setAttribute("yss", yss);
+		
+		return MODULE_NAME+"/yss/edit";
+	}
+	
+	/**
+	 * 跳转到单位管理-运输商-列表页面
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value="/yss/list")
+	public String goYssList(HttpServletRequest request) {
+		
+		//publicService.selectNav(request);
+		
+		return MODULE_NAME+"/yss/list";
+	}
+
+	@RequestMapping(value="/yss/detail")
+	public String goYssDetail(HttpServletRequest request) {
+		
+		//publicService.selectNav(request);
+		String id = request.getParameter("id");
+		YunShuShang yss=yunShuShangService.selectById(id);
+		request.setAttribute("yss", yss);
+		
+		return MODULE_NAME+"/yss/detail";
+	}
 
 	/**
 	 * 跳转到单位管理-发货单位-添加页面
@@ -142,6 +186,77 @@ public class DWGLController {
 		request.setAttribute("shdw", shdw);
 		
 		return MODULE_NAME+"/shdw/detail";
+	}
+
+	@RequestMapping(value="/newYunShuShang")
+	@ResponseBody
+	public Map<String, Object> newYunShuShang(YunShuShang yss) {
+		
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		
+		int count=yunShuShangService.add(yss);
+		if(count>0) {
+			jsonMap.put("message", "ok");
+			jsonMap.put("info", "创建运输商成功！");
+		}
+		else {
+			jsonMap.put("message", "no");
+			jsonMap.put("info", "创建运输商失败！");
+		}
+		return jsonMap;
+	}
+
+	@RequestMapping(value="/deleteYunShuShang",produces="plain/text; charset=UTF-8")
+	@ResponseBody
+	public String deleteYunShuShang(String ids) {
+		//TODO 针对分类的动态进行实时调整更新
+		int count=yunShuShangService.deleteByIds(ids);
+		PlanResult plan=new PlanResult();
+		String json;
+		if(count==0) {
+			plan.setStatus(0);
+			plan.setMsg("删除运输商失败");
+			json=JsonUtil.getJsonFromObject(plan);
+		}
+		else {
+			plan.setStatus(1);
+			plan.setMsg("删除运输商成功");
+			json=JsonUtil.getJsonFromObject(plan);
+		}
+		return json;
+	}
+
+	@RequestMapping(value="/editYunShuShang")
+	@ResponseBody
+	public Map<String, Object> editYunShuShang(YunShuShang yss) {
+		
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		
+		int count=yunShuShangService.edit(yss);
+		if(count>0) {
+			jsonMap.put("message", "ok");
+			jsonMap.put("info", "编辑运输商成功！");
+		}
+		else {
+			jsonMap.put("message", "no");
+			jsonMap.put("info", "编辑运输商失败！");
+		}
+		return jsonMap;
+	}
+
+	@RequestMapping(value="/queryYunShuShangList")
+	@ResponseBody
+	public Map<String, Object> queryYunShuShangList(String mc,int page,int rows,String sort,String order) {
+		
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		
+		int count = yunShuShangService.queryForInt(mc);
+		List<YunShuShang> yssList=yunShuShangService.queryList(mc, page, rows, sort, order);
+		
+		jsonMap.put("total", count);
+		jsonMap.put("rows", yssList);
+		
+		return jsonMap;
 	}
 
 	@RequestMapping(value="/newFaHuoDanWei")
